d82875b0a1791575f1ad9dcb83aa69d0ff70e1cc
RubaXa <ibnRubaXa@gmail.com>
2016-08-18 23:30:46 +0300

    * define/call

--- mode/json.ts
+++ mode/json.ts
@@ -7,6 +7,8 @@ import {
 	KEYWORD_TYPE,
 	TAG_TYPE,
 	HIDDEN_CLASS_TYPE,
+	DEFINE_TYPE,
+	CALL_TYPE,
 } from '../syntax/utils';
 import xtplParser from '../syntax/xtpl';
 import {compile as compileKeyword} from '../src/keywords';
@@ -28,11 +30,6 @@ export interface JSONModeOptions {
 }
 
 export default (options:JSONModeOptions = {}) => (node:Bone) => {
-	let varNum = 0;
-	let maxVarNum = 0;
-	let hasRoot = true;
-	const staticFragments = [];
-
 	function processing(node:Bone):Node {
 		const raw = node.raw || {};
 		const type = node.type;
@@ -66,120 +63,142 @@ export default (options:JSONModeOptions = {}) => (node:Bone) => {
 		};
 	}
 
-	function compile(pad:string, node:Node, rootVar?:string, isStatic?:boolean):string {
-		const {type, name, value, attrs, attrsStr, children, computed, hasComputedAttrs} = node;
-		let code;
+	let varNum = 0;
+	let maxVarNum = 0;
+	const defines = [];
+	const staticFragments = [];
 
-		if (KEYWORD_TYPE === type) {
-			const pair = compileKeyword(name, attrs);
-			let staticPool = [];
-			let flush = () => {
-				if (staticPool.length) {
-					code += `${pad}  ${rootVar}.children.push(${staticPool.join(', ')})\n`;
-				}
-			};
-			
-			code = `${pad}${pair[0]}\n`;
+	function compile(node) {
+		let hasRoot = true;
+		
+		function build(pad:string, node:Node, rootVar?:string, isStatic?:boolean):string {
+			const {type, name, value, attrs, attrsStr, children, computed, hasComputedAttrs} = node;
+			let code;
 
-			children.forEach(child => {
-				const res = compile(`${pad}  `, child, rootVar);
-
-				if (child.computed) {
-					flush();
-					code += res
-				} else {
-					staticPool.push(res);
-				}
-			});
-
-			flush();
-			code += `${pad}${pair[1]}\n`;
-		} else if (TEXT_TYPE === type) {
-			code = value;
-		} else if (COMMENT_TYPE === type) {
-			code = `{tag: "!", children: ${stringify(value)} }`;
-		} else {
-			const length = children.length;
-
-			if (HIDDEN_CLASS_TYPE === type) {
-				// todo: Неоптимальненько! Нужно убрать это звено.
-				code = '{tag: undefined';
-			} else {
-				code = `{tag: ${name}${attrsStr ? `, attrs: {${attrsStr}}` : ''}`;
-			}
-
-			if (length === 1 && children[0].type === TEXT_TYPE) {
-				code += `, children: ${compile('', children[0])}}`;
-			} else if (length) {
-				let staticChildren = true;
-				let localVar;
+			if (KEYWORD_TYPE === type) {
+				const pair = compileKeyword(name, attrs);
+				let staticPool = [];
+				let flush = () => {
+					if (staticPool.length) {
+						code += `${pad}  ${rootVar}.children.push(${staticPool.join(', ')})\n`;
+					}
+				};
 				
-				code += ', children: ' + (length > 1 || computed ? '[' : '');
-				
-				children.forEach((child, i) => {
+				code = `${pad}${pair[0]}\n`;
+
+				children.forEach(child => {
+					const res = build(`${pad}  `, child, rootVar);
+
 					if (child.computed) {
-						if (staticChildren) {
-							localVar = `__V${varNum++}`;
-							staticChildren = false;
-							
-							code = `${pad}${localVar} = ${code}]}\n${pad}${rootVar}.children.push(${localVar})\n`;
-						}
-
-						code += compile(pad, child, localVar);
+						flush();
+						code += res
 					} else {
-						const next = compile(pad, child, null, !computed && !hasComputedAttrs);
-
-						if (staticChildren) {
-							code += (i ? ', ' : '') + next;
-						} else {
-							code += `${pad}${rootVar}.children.push(${next})\n`;
-						}
+						staticPool.push(res);
 					}
 				});
 
-				if (staticChildren) {
-					code += (length > 1 ? ']' : '') + '}';
-				} else {
-					maxVarNum = Math.max(maxVarNum, varNum);
-					varNum--;
-				}
+				flush();
+				code += `${pad}${pair[1]}\n`;
+			} else if (TEXT_TYPE === type) {
+				code = value;
+			} else if (COMMENT_TYPE === type) {
+				code = `{tag: "!", children: ${stringify(value)} }`;
+			} else if (DEFINE_TYPE === type) {
+				defines.push([
+					`function ${name}(${attrs.join(', ')}) {`,
+					compile(children[0]),
+					`}`
+				].join('\n'));
+				
+				code = '';
 			} else {
-				code += '}';
+				const length = children.length;
+
+				if (HIDDEN_CLASS_TYPE === type) {
+					// todo: Неоптимальненько! Нужно убрать это звено.
+					code = '{tag: undefined';
+				} else {
+					code = `{tag: ${name}${attrsStr ? `, attrs: {${attrsStr}}` : ''}`;
+				}
+
+				if (length === 1 && children[0].type === TEXT_TYPE) {
+					code += `, children: ${build('', children[0])}}`;
+				} else if (length) {
+					let staticChildren = true;
+					let localVar;
+					
+					code += ', children: ' + (length > 1 || computed ? '[' : '');
+					
+					children.forEach((child, i) => {
+						if (child.computed) {
+							if (staticChildren) {
+								localVar = `__V${varNum++}`;
+								staticChildren = false;
+								
+								code = `${pad}${localVar} = ${code}]}\n${pad}${rootVar}.children.push(${localVar})\n`;
+							}
+
+							code += build(pad, child, localVar);
+						} else {
+							const next = build(pad, child, null, !computed && !hasComputedAttrs);
+
+							if (staticChildren) {
+								code += (i ? ', ' : '') + next;
+							} else {
+								code += `${pad}${rootVar}.children.push(${next})\n`;
+							}
+						}
+					});
+
+					if (staticChildren) {
+						code += (length > 1 ? ']' : '') + '}';
+					} else {
+						maxVarNum = Math.max(maxVarNum, varNum);
+						varNum--;
+					}
+				} else {
+					code += '}';
+				}
+
+				if (!computed && !isStatic && !hasComputedAttrs) {
+					code = `__S${staticFragments.push(code)}`;
+					
+					if (rootVar === '__ROOT') {
+						hasRoot = false;
+						code = `return ${code}`;
+					}
+				}
 			}
 
-			if (!computed && !isStatic && !hasComputedAttrs) {
-				code = `__S${staticFragments.push(code)}`;
-				
-				if (rootVar === '__ROOT') {
-					hasRoot = false;
-					code = `return ${code}`;
-				}
-			}
+			return code;
+		}
+
+		let code = build('', node, '__ROOT').trim();	
+
+		if (hasRoot) {
+			let exportName;
+			
+			// Наверно есть способ лучше, но мне его лень искать, пусть так
+			code = code.replace(/__ROOT\.children\.push\((.*?)\)\n/, (_, name) => {
+				exportName = name;
+				return '';
+			});
+
+			code = exportName ? `${code}\nreturn ${exportName}` : `return (${code})`;
 		}
 
 		return code;
 	}
 
-	let code = compile('', processing(node), '__ROOT').trim();	
+	let code = compile(processing(node));
+
 	let vars = [].concat(
 		Array.apply(null, Array(maxVarNum)).map((_, i) => `__V${i}`),
 		staticFragments.map((code, i) => `__S${i+1} = ${code}`)
 	);
 
-	if (hasRoot) {
-		let exportName;
-		
-		// Наверно есть способ лучше, но мне его лень искать, пусть так
-		code = code.replace(/__ROOT\.children\.push\((.*?)\)\n/, (_, name) => {
-			exportName = name;
-			return '';
-		});
-
-		code = exportName ? `${code}\nreturn ${exportName}` : `return (${code})`;
-	}
-
 	return {
-		before: (vars.length ? `var ${vars.join(', ')};\n` : ''),
+		before: defines.join('\n') + '\n' + (vars.length ? `var ${vars.join(', ')};\n` : ''),
 		code
 	};
 };
